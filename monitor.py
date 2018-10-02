e711eb20c083d75534ec2a4dfc834bb7e9c461f5
Josh Peak <neoenith.dev@gmail.com>
2018-10-02 21:30:40 +1000

    start serial commands

--- monitor.py
+++ monitor.py
@@ -1,3 +1,19 @@
 #! /usr/bin/env python3
 
-print("hello")
+import serial
+
+
+def main():
+  print("hello")
+  ser= serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+  )
+
+
+if __name__ == "__main__":
+  main()
